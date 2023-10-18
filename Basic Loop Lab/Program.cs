545da602352f73d81ac8c19a40b8769f204bd2bc
Benjamin Holmes <manbej.sholem@gmail.com>
2023-10-18 16:25:31 -0400

    Add project files.

---
+++ Basic Loop Lab/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Basic_Loop_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateSumOfNumbers();
+        }
+
+        static void CalculateSumOfNumbers()
+        {
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine();
+            int inputInt = int.Parse(input);
+
+            int total = 0;
+            for (int i = 0 ; i <= inputInt; i++) 
+            {
+                total += i;
+                
+            }
+            Console.WriteLine($"The sum from 0 to {inputInt} is {total}");
+        }
+    }
+}
